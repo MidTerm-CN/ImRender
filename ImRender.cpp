f68559eb5988ec5ed425f8549c6a927654e63abe
MidTerm-CN <83219310+MidTerm-CN@users.noreply.github.com>
2023-04-27 16:54:04 +0800

    Remove CStyleAnalysisSystem

--- ImRender.cpp
+++ ImRender.cpp
@@ -3,9 +3,9 @@
 #include <ctype.h>
 #include <string>
 
-std::unordered_map<std::string, std::string> CStyleAnalysisSystem::AnalysisStyleMap(const std::string& style)
+
+ImRenderStyleBase::ImRenderStyleBase(const std::string& style)
 {
-    std::unordered_map<std::string, std::string> styleMap;
     std::string::size_type pos = 0;
     std::string::size_type prePos = 0;
     while ((pos = style.find(";", prePos)) != std::string::npos)
@@ -15,54 +15,55 @@ std::unordered_map<std::string, std::string> CStyleAnalysisSystem::AnalysisStyle
         std::string value = style.substr(equalPos + 1, pos - equalPos - 1);
         std::transform(key.begin(), key.end(), key.begin(), ::tolower);
         std::transform(value.begin(), value.end(), value.begin(), ::tolower);
-        styleMap[key] = value;
+        m_styleMap[key] = value;
         prePos = pos + 1;
     }
-    return styleMap;
 }
 
-ImRenderVerticalAxisDockType CStyleAnalysisSystem::AnalysisVerticalAxisDockType(std::unordered_map<std::string, std::string> styleMap)
+ImRenderVerticalAxisDockType ImRenderStyleBase::GetVerticalAxisDockType()
 {
-    if (styleMap.find("vertical") != styleMap.end())
+    if (m_styleMap.find("vertical") != m_styleMap.end())
     {
-        std::string vertical = styleMap["vertical"];
+        std::string vertical = m_styleMap["vertical"];
         if (vertical.compare("top") == 0) return ImRenderVerticalAxisDockType::Top;
         else if (vertical.compare("bottom") == 0) return ImRenderVerticalAxisDockType::Bottom;
         else if (vertical.compare("center") == 0) return ImRenderVerticalAxisDockType::Center;
     }
+    return ImRenderVerticalAxisDockType::Bottom;
 }
 
-ImRenderHorizontalAxisDockType CStyleAnalysisSystem::AnalysisHorizontalAxisDockType(std::unordered_map<std::string, std::string> styleMap)
+ImRenderHorizontalAxisDockType ImRenderStyleBase::GetHorizontalAxisDockType()
 {
-    if (styleMap.find("horizontal") != styleMap.end())
+    if (m_styleMap.find("horizontal") != m_styleMap.end())
     {
-        std::string horizontal = styleMap["horizontal"];
+        std::string horizontal = m_styleMap["horizontal"];
         if (horizontal.compare("left") == 0) return ImRenderHorizontalAxisDockType::Left;
         else if (horizontal.compare("right") == 0) return ImRenderHorizontalAxisDockType::Right;
         else if (horizontal.compare("center") == 0) return ImRenderHorizontalAxisDockType::Center;
     }
+    return ImRenderHorizontalAxisDockType::Right;
 }
 
-bool CStyleAnalysisSystem::AnalysisBool(std::unordered_map<std::string, std::string> styleMap, const std::string& key, bool defaultValue)
+bool ImRenderStyleBase::GetBool(const std::string& key, bool defaultValue)
 {
     std::string lowerKey = key;
     std::transform(lowerKey.begin(), lowerKey.end(), lowerKey.begin(), ::tolower);
-    if (styleMap.find(lowerKey) != styleMap.end())
+    if (m_styleMap.find(lowerKey) != m_styleMap.end())
     {
-        std::string value = styleMap[lowerKey];
+        std::string value = m_styleMap[lowerKey];
         if (value.compare("true") == 0) return true;
         else if (value.compare("false") == 0) return false;
     }
     return defaultValue;
 }
 
-float CStyleAnalysisSystem::AnalysisFloat(std::unordered_map<std::string, std::string> styleMap, const std::string& key, float defaultValue)
+float ImRenderStyleBase::GetFloat(const std::string& key, float defaultValue)
 {
     std::string lowerKey = key;
     std::transform(lowerKey.begin(), lowerKey.end(), lowerKey.begin(), ::tolower);
-    if (styleMap.find(lowerKey) != styleMap.end())
+    if (m_styleMap.find(lowerKey) != m_styleMap.end())
     {
-        std::string value = styleMap[lowerKey];
+        std::string value = m_styleMap[lowerKey];
         try
         {
             return std::stof(value);
@@ -72,76 +73,10 @@ float CStyleAnalysisSystem::AnalysisFloat(std::unordered_map<std::string, std::s
     return defaultValue;
 }
 
-ImRenderTextStyle CStyleAnalysisSystem::AnalysisTextStyle(std::string& style)
-{
-    ImRenderTextStyle textStyle;
-    textStyle.vertical = ImRenderVerticalAxisDockType::Bottom;
-    textStyle.horizontal = ImRenderHorizontalAxisDockType::Right;
-    textStyle.outline = false;
-    textStyle.strikethrough = false;
-    textStyle.underline = false;
-    textStyle.sizeScale = 1.f;
-    if (style.empty())  return textStyle;
-    std::unordered_map<std::string, std::string> styleMap = AnalysisStyleMap(style);
-    textStyle.vertical = AnalysisVerticalAxisDockType(styleMap);
-    textStyle.horizontal = AnalysisHorizontalAxisDockType(styleMap);
-    textStyle.outline = AnalysisBool(styleMap, "Outline");
-    textStyle.strikethrough = AnalysisBool(styleMap, "Strikethrough");
-    textStyle.underline = AnalysisBool(styleMap, "Underline");
-    textStyle.sizeScale = AnalysisFloat(styleMap, "SizeScale", 1.f);
-    return textStyle;
-}
-
-ImRenderRectStyle CStyleAnalysisSystem::AnalysisRectStyle(std::string& style)
-{
-    ImRenderRectStyle rectStyle;
-    rectStyle.vertical = ImRenderVerticalAxisDockType::Bottom;
-    rectStyle.horizontal = ImRenderHorizontalAxisDockType::Right;
-    rectStyle.outline = false;
-    rectStyle.fill = false;
-    rectStyle.thickness = 1.0f;
-    rectStyle.rounding = 0.0f;
-    if (style.empty())  return rectStyle;
-    std::unordered_map<std::string, std::string> styleMap = AnalysisStyleMap(style);
-    rectStyle.vertical = AnalysisVerticalAxisDockType(styleMap);
-    rectStyle.horizontal = AnalysisHorizontalAxisDockType(styleMap);
-    rectStyle.outline = AnalysisBool(styleMap, "Outline");
-    rectStyle.fill = AnalysisBool(styleMap, "Fill");
-    rectStyle.thickness = AnalysisFloat(styleMap, "Thickness", 1.f);
-    rectStyle.rounding = AnalysisFloat(styleMap, "Rounding");
-    return rectStyle;
-}
-
-ImRenderLineStyle CStyleAnalysisSystem::AnalysisLineStyle(std::string& style)
-{
-    ImRenderLineStyle lineStyle;
-    lineStyle.thickness = 1.0f;
-    lineStyle.outline = false;
-    if (style.empty())  return lineStyle;
-    std::unordered_map<std::string, std::string> styleMap = AnalysisStyleMap(style);
-    lineStyle.thickness = AnalysisFloat(styleMap, "Thickness", 1.f);
-    lineStyle.outline = AnalysisBool(styleMap, "Outline");
-    return lineStyle;
-}
-
-ImRenderCircleStyle CStyleAnalysisSystem::AnalysisCircleStyle(std::string& style)
-{
-    ImRenderCircleStyle circleStyle;
-    circleStyle.outline = false;
-    circleStyle.fill = false;
-    circleStyle.thickness = 1.0f;
-    if (style.empty())  return circleStyle;
-    std::unordered_map<std::string, std::string> styleMap = AnalysisStyleMap(style);
-    circleStyle.outline = AnalysisBool(styleMap, "Outline");
-    circleStyle.fill = AnalysisBool(styleMap, "Fill");
-    circleStyle.thickness = AnalysisFloat(styleMap, "Thickness", 1.f);
-    return circleStyle;
-}
-
-ImVec2 CImRender::Text(std::string text, ImVec2 pos, ImVec4 color, std::string style)
+ImVec2 CImRender::Text(std::string text, ImVec2 pos, ImVec4 color, const std::string& style)
 {
     m_pDrawList = ImGui::GetBackgroundDrawList();
-    ImRenderTextStyle textStyle = CStyleAnalysisSystem::AnalysisTextStyle(style);
+    ImRenderTextStyle textStyle(style);
     ImGuiContext& g = *GImGui;
     const float font_size = g.FontSize * textStyle.sizeScale;
     ImFont* font = g.Font;
@@ -170,10 +105,10 @@ ImVec2 CImRender::Text(std::string text, ImVec2 pos, ImVec4 color, std::string s
     return textSize;
 }
 
-void CImRender::Rect(ImVec2 pos, ImVec2 size, ImVec4 color, std::string style)
+void CImRender::Rect(ImVec2 pos, ImVec2 size, ImVec4 color, const std::string& style)
 {
     m_pDrawList = ImGui::GetBackgroundDrawList();
-    ImRenderRectStyle retctStyle = CStyleAnalysisSystem::AnalysisRectStyle(style);
+	ImRenderRectStyle retctStyle(style);
     ImVec2 renderPos = pos;
     if (retctStyle.vertical == ImRenderVerticalAxisDockType::Top)
         renderPos.y -= size.y;
@@ -200,19 +135,19 @@ void CImRender::Rect(ImVec2 pos, ImVec2 size, ImVec4 color, std::string style)
     }
 }
 
-void CImRender::Line(ImVec2 pos1, ImVec2 pos2, ImVec4 color, std::string style)
+void CImRender::Line(ImVec2 pos1, ImVec2 pos2, ImVec4 color, const std::string& style)
 {
     m_pDrawList = ImGui::GetBackgroundDrawList();
-    ImRenderLineStyle lineStyle = CStyleAnalysisSystem::AnalysisLineStyle(style);
+    ImRenderLineStyle lineStyle(style);
     if (lineStyle.outline)
         m_pDrawList->AddLine(pos1, pos2, IM_COL32_BLACK, lineStyle.thickness + 2);
     m_pDrawList->AddLine(pos1, pos2, ImGui::GetColorU32(color), lineStyle.thickness);
 }
 
-void CImRender::Circle(ImVec2 pos, float radius, ImVec4 color, std::string style)
+void CImRender::Circle(ImVec2 pos, float radius, ImVec4 color, const std::string& style)
 {
     m_pDrawList = ImGui::GetBackgroundDrawList();
-    ImRenderCircleStyle circleStyle = CStyleAnalysisSystem::AnalysisCircleStyle(style);
+    ImRenderCircleStyle circleStyle(style);
     if (circleStyle.fill)
     {
         if (circleStyle.outline)
