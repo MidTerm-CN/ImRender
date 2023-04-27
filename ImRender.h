a36a73d3b726a8041286058e36c4c7cb25b37d40
MidTerm-CN <83219310+MidTerm-CN@users.noreply.github.com>
2023-04-27 16:53:40 +0800

    Remove CStyleAnalysisSystem

--- ImRender.h
+++ ImRender.h
@@ -1,7 +1,12 @@
 #pragma once
-#include "imrender_example/imgui/imgui.h"
-#include "imrender_example/imgui/imgui_internal.h"
+#include <imgui.h>
+#include <imgui_internal.h>
 #include <unordered_map>
+
+#define IMRENDER_STYLE_VALUE_VERTICALDOCKTYPE ImRenderVerticalAxisDockType vertical = this->GetVerticalAxisDockType()
+#define IMRENDER_STYLE_VALUE_HORIZONTALDOCKTYPE ImRenderHorizontalAxisDockType horizontal = this->GetHorizontalAxisDockType()
+#define IMRENDER_STYLE_VALUE_BOOL(key,defaultValue) bool key = this->GetBool(#key,defaultValue)
+#define IMRENDER_STYLE_VALUE_FLOAT(key,defaultValue) float key = this->GetFloat(#key,defaultValue)
 
 enum class ImRenderVerticalAxisDockType
 {
@@ -17,51 +22,53 @@ enum class ImRenderHorizontalAxisDockType
     Center
 };
 
-struct ImRenderTextStyle
+struct ImRenderStyleBase
 {
-    ImRenderVerticalAxisDockType vertical;
-    ImRenderHorizontalAxisDockType horizontal;
-    bool outline;
-    bool strikethrough;
-    bool underline;
-    float sizeScale;
-};
-
-struct ImRenderRectStyle
-{
-    ImRenderVerticalAxisDockType vertical;
-    ImRenderHorizontalAxisDockType horizontal;
-    bool outline;
-    bool fill;
-    float thickness;
-    float rounding;
-};
-
-struct ImRenderLineStyle
-{
-    float thickness;
-    bool outline;
-};
-
-struct ImRenderCircleStyle
-{
-    bool outline;
-    bool fill;
-    float thickness;
-};
-
-class CStyleAnalysisSystem
-{
+private:
+    std::unordered_map<std::string, std::string> m_styleMap;
 public:
-    static std::unordered_map<std::string, std::string> AnalysisStyleMap(const std::string& style);
-    static ImRenderVerticalAxisDockType AnalysisVerticalAxisDockType(std::unordered_map<std::string, std::string> styleMap);
-    static ImRenderHorizontalAxisDockType AnalysisHorizontalAxisDockType(std::unordered_map<std::string, std::string> styleMap);
-    static bool AnalysisBool(std::unordered_map<std::string, std::string> styleMap, const std::string& key, bool defaultValue = false);
-    static float AnalysisFloat(std::unordered_map<std::string, std::string> styleMap, const std::string& key, float defaultValue = 0.f);
-    static ImRenderTextStyle AnalysisTextStyle(std::string& style);
-    static ImRenderRectStyle AnalysisRectStyle(std::string& style);
-    static ImRenderLineStyle AnalysisLineStyle(std::string& style);
-    static ImRenderCircleStyle AnalysisCircleStyle(std::string& style);
+	ImRenderStyleBase(const std::string& style);
+    ImRenderVerticalAxisDockType GetVerticalAxisDockType();
+    ImRenderHorizontalAxisDockType GetHorizontalAxisDockType();
+    bool GetBool(const std::string& key, bool defaultValue);
+    float GetFloat(const std::string& key, float defaultValue);
+};
+
+struct ImRenderTextStyle : public ImRenderStyleBase
+{
+	ImRenderTextStyle(const std::string& style) : ImRenderStyleBase(style) {}
+    IMRENDER_STYLE_VALUE_VERTICALDOCKTYPE;
+    IMRENDER_STYLE_VALUE_HORIZONTALDOCKTYPE;
+    IMRENDER_STYLE_VALUE_BOOL(outline, false);
+	IMRENDER_STYLE_VALUE_BOOL(strikethrough, false);
+	IMRENDER_STYLE_VALUE_BOOL(underline, false);
+	IMRENDER_STYLE_VALUE_FLOAT(sizeScale, 1.f);
+};
+
+struct ImRenderRectStyle : public ImRenderStyleBase
+{
+	ImRenderRectStyle(const std::string& style) : ImRenderStyleBase(style) {}
+    IMRENDER_STYLE_VALUE_VERTICALDOCKTYPE;
+    IMRENDER_STYLE_VALUE_HORIZONTALDOCKTYPE;
+	IMRENDER_STYLE_VALUE_BOOL(outline, false);
+	IMRENDER_STYLE_VALUE_BOOL(fill, false);
+	IMRENDER_STYLE_VALUE_FLOAT(thickness, 1.f);
+	IMRENDER_STYLE_VALUE_FLOAT(rounding, 0.f);
+};
+
+struct ImRenderLineStyle : public ImRenderStyleBase
+{
+	ImRenderLineStyle(const std::string& style) : ImRenderStyleBase(style) {}
+	IMRENDER_STYLE_VALUE_FLOAT(thickness, 1.f);
+	IMRENDER_STYLE_VALUE_BOOL(outline, false);
+};
+
+struct ImRenderCircleStyle : public ImRenderStyleBase
+{
+	ImRenderCircleStyle(const std::string& style) : ImRenderStyleBase(style) {}
+	IMRENDER_STYLE_VALUE_BOOL(outline, false);
+	IMRENDER_STYLE_VALUE_BOOL(fill, false);
+	IMRENDER_STYLE_VALUE_FLOAT(thickness, 1.f);
 };
 
 class CImRender
@@ -69,10 +76,15 @@ class CImRender
 private:
     ImDrawList* m_pDrawList;
 public:
-    virtual ImVec2 Text(std::string text, ImVec2 pos, ImVec4 color, std::string style);
-    virtual void Rect(ImVec2 pos, ImVec2 size, ImVec4 color, std::string style);
-    virtual void Line(ImVec2 pos1, ImVec2 pos2, ImVec4 color, std::string style);
-    virtual void Circle(ImVec2 pos, float radius, ImVec4 color, std::string style);
+    ImVec2 Text(std::string text, ImVec2 pos, ImVec4 color, const std::string& style);
+    void Rect(ImVec2 pos, ImVec2 size, ImVec4 color, const std::string& style);
+    void Line(ImVec2 pos1, ImVec2 pos2, ImVec4 color, const std::string& style);
+    void Circle(ImVec2 pos, float radius, ImVec4 color, const std::string& style);
 };
 
 inline CImRender* ImRender = new CImRender();
+
+#undef IMRENDER_STYLE_VALUE_VERTICALDOCKTYPE
+#undef IMRENDER_STYLE_VALUE_HORIZONTALDOCKTYPE
+#undef IMRENDER_STYLE_VALUE_BOOL
+#undef IMRENDER_STYLE_VALUE_FLOAT
